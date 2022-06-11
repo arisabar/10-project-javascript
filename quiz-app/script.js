ec5010968fa3378f7985ccdbd11e033f8b71f678
arisabar <arisabar_subakti@yahoo.co.id>
2022-06-11 10:58:38 +0800

    quiz app

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -37,11 +37,13 @@ const quizData = [
     b: "1995",
     c: "1994",
     d: "none of the above",
-    correct: "d",
+    correct: "b",
   },
 ];
 
 const questionEl = document.getElementById("question");
+const showQuiz = document.getElementById("quiz");
+const answerEl = document.querySelectorAll(".answer");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
@@ -49,10 +51,13 @@ const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
+// let answer = undefined;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+  deselectAnswers();
   const currentQuizData = quizData[currentQuiz];
   questionEl.innerText = currentQuizData.question;
   a_text.innerText = currentQuizData.a;
@@ -62,14 +67,40 @@ function loadQuiz() {
   //   currentQuestion++;
 }
 
-function getSelected() {}
+function getSelected() {
+  let answer = undefined;
+
+  answerEl.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+
+function deselectAnswers() {
+  answerEl.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
 
 submitBtn.addEventListener("click", () => {
-  currentQuiz++;
-  if (currentQuiz < quizData.length) {
-    loadQuiz();
-  } else {
-    alert("You finished! Get yourself an Orange Lemonande");
+  //* check to see the answer
+  const answer = getSelected();
+
+  // console.log(answer);
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      // showQuiz.innerHTML = `You finished! your score at ${score}/${quizData.length}`;
+      confirm(`You finished! your score at ${score}/${quizData.length}`);
+      window.location.reload();
+    }
   }
-  //   loadQuiz();
 });
